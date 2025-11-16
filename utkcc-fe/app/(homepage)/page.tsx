4cd8c4e6e3da84dfacad4bfa11f9f81fb510ae88
caelankim <137230959+caelankim@users.noreply.github.com>
2025-11-15 22:25:56 -0500

    Update page.tsx

--- utkcc-fe/app/(homepage)/page.tsx
+++ utkcc-fe/app/(homepage)/page.tsx
@@ -1,29 +1,16 @@
-import Entry from './entry';
-import About from '../(subpages)/about/page';
-import Executives from '../(subpages)/executives/page';
-import Sponsors from '../(subpages)/sponsors/page';
-import Resources from '../(subpages)/resources/page';
-import Newsletter from '../(subpages)/newsletter/page';
-import EventsHomeIntro from '../(subpages)/events/eventsHomeIntro';
-
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Entry />
+
       {/* default: px-8 */}
       <div className="pl-safe-left pr-safe-right">
-        <div className="px-4 lg:px-32 z-50 flex flex-col gap-y-[15vh] mb-[10vh]">
-          {/* about intro */}
+        <div className="px-4 lg:px-20 z-50 flex flex-col gap-y-[10vh] mb-[5vh]">
           <About />
-          {/* events intro */}
           <EventsHomeIntro />
-          {/* exec intro */}
           <Executives />
-          {/* sponsors intro */}
           <Sponsors />
-          {/* resources intro */}
           <Resources />
-          {/* newsletter intro */}
           <Newsletter />
         </div>
       </div>
